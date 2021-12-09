55d160b9cd65a06e1adfa6741d2e2666e5a0f9e6
sanxoxo <95870850+sanxoxo@users.noreply.github.com>
2021-12-09 18:45:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,9 +25,13 @@ async def register_customer(payload: Request):
     print(company_results)
     # Step 2: create a new customer:
     query_customer = database.execute('''
-            INSERT INTO Customer (CustomerAccount, VAT)
-            VALUES ({customer}, {company})
-            '''.format(customer=str(values_dict['CustomerAccount']) ,company=str(values_dict['VAT'])))
+            INSERT INTO Customer (CustomerAccount, VAT, FullName, Email, Password)
+            VALUES ({customer}, {company}, {fullname}, {email}, {password})
+            '''.format(customer=str(values_dict['CustomerAccount']) ,
+                       company=str(values_dict['VAT']),
+                       fullname=values_dict['FullName'],
+                       email=str(values_dict['Email']),
+                       password=str(values_dict['Password'])))
     print (query_customer)
     # Close the DB
     database.close()
@@ -59,29 +63,6 @@ async def register_subscription(payload: Request):
     # Close the DB
     database.close()
     return True
-#--------------------------------------------------------
-# create a quote for each customer (via subscriptions)    PAS DEFINITIF
-#---------------------------------------------------------
-  @app.post("/register_quote")
-async def register_Quote(payload: Request):
-    values_dict = await payload.json()
-    # Open the DB
-    database = sqlite3.connect('databaseproject.db', isolation_level=None)
-    # Step 1: retrieve the customer
-
-    query_customer = database.execute(''' 
-                    SELECT CustomerAccount FROM Customer
-                    WHERE VAT = {}               
-                    '''.format(str(values_dict['VAT'])))
-    # We then store the results of the query with fetchall.
-    customer_results = query_customer.fetchall()
-    print(customer_results)
-    # Step 2: create a new quote for the customer and subscription:
-    #query_quote = database.execute('''
-            #INSERT INTO Quote(CustomerAccount, SubscriptionN) 
-            #VALUES ({customer}, {subscription})             
-            #'''.format(customer=str(customer_results), subscription=str(values_dict['SubscriptionN'])))
-    #print(query_quote)
-    # Close the DB
-    database.close()
-    return True
+  #--------------------------------------------------------
+  # create a quote for each customer    PAS DEFINITIF
+  #---------------------------------------------------------
